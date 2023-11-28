0615566fc8471b2e47b68178c79ab4baa58a1f3d
Eric Shahan <ctibalt@gmail.com>
2023-11-27 19:39:50 -0600

    Set up .gradle + spring

---
+++ src/main/java/org/launchcode/buildMyApptriangle/BuildMyAppTriangleApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.buildMyApptriangle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildMyAppTriangleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildMyAppTriangleApplication.class, args);
+	}
+
+}
