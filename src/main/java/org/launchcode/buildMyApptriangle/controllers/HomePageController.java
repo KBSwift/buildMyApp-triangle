ae0d0877b779b099304066a703649982f2bbaaa2
jeanOlele <84257608+jeanOlele@users.noreply.github.com>
2023-12-18 10:42:12 -0600

    Display welcome  message from same branch

--- src/main/java/org/launchcode/buildMyApptriangle/controllers/HomePageController.java
+++ src/main/java/org/launchcode/buildMyApptriangle/controllers/HomePageController.java
@@ -1,2 +1,15 @@
-package org.launchcode.buildMyApptriangle.controllers;public class HomePageController {
+package org.launchcode.buildMyApptriangle.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomePageController {
+    @GetMapping("/")
+    @ResponseBody
+    public String renderHomePage() {
+        return "WELCOME TO OUR CRM BUILD MY APP TRIANGLE APPLICATION";
+    }
 }
