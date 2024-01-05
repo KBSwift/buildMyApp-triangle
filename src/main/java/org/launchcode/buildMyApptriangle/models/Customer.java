26f9ac79a0444617436f02a705a839d25906ce9c
Seth Woodrow <seth.woodrow0@gmail.com>
2024-01-04 19:57:01 -0600

    reworked the customer class

--- src/main/java/org/launchcode/buildMyApptriangle/models/Customer.java
+++ src/main/java/org/launchcode/buildMyApptriangle/models/Customer.java
@@ -1,8 +1,10 @@
 package org.launchcode.buildMyApptriangle.models;
 
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.List;
 public class Customer extends AbstractEntity{
 
 
@@ -13,8 +15,8 @@ public class Customer extends AbstractEntity{
     @Size(min =1,max = 10)
     private String customerPhoneNumber;
 
-
-
+    @OneToMany(mappedBy = "customer")
+    private List<Contract> contractList;
 
     public String getCustomerEmail() {
         return customerEmail;
@@ -31,6 +33,11 @@ public class Customer extends AbstractEntity{
     public void setCustomerPhoneNumber(String customerPhoneNumber) {
         this.customerPhoneNumber = customerPhoneNumber;
     }
+    public List<Contract> getContractList(){
+        return contractList;
+    }
 
-
+    public void setContractList(List<Contract> contractList){
+        this.contractList=contractList;
+    }
 }
