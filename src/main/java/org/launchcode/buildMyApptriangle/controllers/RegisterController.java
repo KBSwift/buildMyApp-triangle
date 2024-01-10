e8efd3932d150f48c5499213837e07108e232c39
Eric Shahan <ctibalt@gmail.com>
2024-01-10 12:28:52 -0600

    Configured Web Security to Enable Data Transfer to database.
    TODO: Properly display Error messages. Add Further Authentication to prevent duplicate accounts.

--- src/main/java/org/launchcode/buildMyApptriangle/controllers/RegisterController.java
+++ src/main/java/org/launchcode/buildMyApptriangle/controllers/RegisterController.java
@@ -1,17 +1,12 @@
 package org.launchcode.buildMyApptriangle.controllers;
 
-import jakarta.validation.Valid;
 import org.launchcode.buildMyApptriangle.models.User;
-import org.launchcode.buildMyApptriangle.models.data.UserRepository;
 import org.launchcode.buildMyApptriangle.security.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,24 +14,10 @@ import java.util.Map;
 
 @Controller
 public class RegisterController {
-//    @Autowired
-//    private UserRepository userRepository;
     @Autowired
     private MyUserDetailsService userDetailsService;
     @Autowired
     private PasswordEncoder passwordEncoder;
-
-//    @GetMapping("register")
-//    public String register(Model model) {
-//        return "register";
-//    }
-//
-//    @PostMapping("register")
-//    public void addUser(@RequestParam String username, @RequestParam String password, Model model) {
-//        User user = new User(username, password);
-//        model.addAttribute("user", user);
-//        userRepository.save(user);
-//    }
 
     @GetMapping("register")
     public String register() {
@@ -45,6 +26,7 @@ public class RegisterController {
 
     @PostMapping(
             value = "/register",
+            // In order to export to database when encrypted, the data has to be changed to a specific type.
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = {
             MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
     )
--- src/main/java/org/launchcode/buildMyApptriangle/models/data/UserRepository.java
+++ src/main/java/org/launchcode/buildMyApptriangle/models/data/UserRepository.java
@@ -1,12 +1,12 @@
 package org.launchcode.buildMyApptriangle.models.data;
 
 import org.launchcode.buildMyApptriangle.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, String> {
     Optional<User> findUserByUsername(String username);
 }
