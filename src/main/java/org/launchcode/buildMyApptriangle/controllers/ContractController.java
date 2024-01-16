633de6a8f0c3896d2a4c52307662d1fbf74ca893
Eric Shahan <ctibalt@gmail.com>
2024-01-15 22:59:02 -0600

    Finished Basic Add Functionality

--- src/main/java/org/launchcode/buildMyApptriangle/controllers/ContractController.java
+++ src/main/java/org/launchcode/buildMyApptriangle/controllers/ContractController.java
@@ -1,10 +1,15 @@
 package org.launchcode.buildMyApptriangle.controllers;
 
+import jakarta.validation.Valid;
+import org.launchcode.buildMyApptriangle.models.Contract;
 import org.launchcode.buildMyApptriangle.models.data.ContractRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,8 +26,21 @@ public class ContractController {
     }
 
     @GetMapping("add")
-    public String displayAddContractForm() {
+    public String displayAddContractForm(Model model) {
+        model.addAttribute(new Contract());
         return "contracts/add";
+    }
+
+    @PostMapping("add")
+    public String processAddContractForm(@ModelAttribute @Valid Contract newContract,
+                                         Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "contracts/add";
+        }
+        else {
+            contractRepository.save(newContract);
+        }
+        return "redirect:";
     }
 
     @GetMapping("delete")
